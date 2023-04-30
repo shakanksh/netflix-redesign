fb4f76ce6547c6e733b8b9077ba9ac7bec1ec3c4
Shakanksh <shakankshs@gmail.com>
2023-04-30 11:20:41 +0530

    Initial Build and Repo Added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+	},
+	domains: ["image.tmdb.org"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
